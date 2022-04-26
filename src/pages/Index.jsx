26e6132c994bb453d24db9c73b7fef2a573bba3e
NNICEEt <niceteerapat@gmail.com>
2022-04-26 21:54:30 +0700

    fetching server to start

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,13 +1,15 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { PanelContext } from "../contexts/panel";
+
 import rsaService from "../services/rsaService";
 import ceasarService from "../services/ceasarService";
 import railFenceCipher from "../services/railFenceService";
 import vigenereService from "../services/viginereService";
-import RSA from "../pages/RSA";
 
+import { BASE_URL } from "../services/apiConstants";
 /*---------------------------------------- Component ----------------------------------------*/
 import Form from "./Form";
+import RSA from "../pages/RSA";
 
 /*---------------------------------------- UI Component ----------------------------------------*/
 import { Box } from "@chakra-ui/react";
@@ -18,20 +20,34 @@ import { panel } from "../utils/panel";
 const Index = () => {
   const { selectedPanel } = useContext(PanelContext);
 
+  useEffect(() => {
+    fetch(BASE_URL);
+  }, []);
+
   return (
     <Box p={5}>
       {selectedPanel === panel.caesarCipher && (
-        <Form cipher={ceasarService} type={"number"} name={panel.caesarCipher} />
+        <Form
+          cipher={ceasarService}
+          type={"number"}
+          name={panel.caesarCipher}
+        />
       )}
       {selectedPanel === panel.vigenereCipher && (
-        <Form cipher={vigenereService} type={"text"} name={panel.vigenereCipher} />
+        <Form
+          cipher={vigenereService}
+          type={"text"}
+          name={panel.vigenereCipher}
+        />
       )}
       {selectedPanel === panel.railFenceCipher && (
-        <Form cipher={railFenceCipher} type={"number"} name={panel.railFenceCipher} />
+        <Form
+          cipher={railFenceCipher}
+          type={"number"}
+          name={panel.railFenceCipher}
+        />
       )}
-      {selectedPanel === panel.rsa && 
-        <RSA cipher={rsaService}/>
-      }
+      {selectedPanel === panel.rsa && <RSA cipher={rsaService} />}
     </Box>
   );
 };
